96af7040647b1313895d8b0c4fc7dde3ad05982f
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2025-11-09 13:47:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.42%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -12,9 +12,7 @@ class Solution(object):
         """
         slow = head
         fast = head
-
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
-        return slow  # slow now points to the middle node
+        return slow
