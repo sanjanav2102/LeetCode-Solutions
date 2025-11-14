57cdd81dc0f77a5d2dfca2038e0999d2c3e168a2
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2025-11-14 18:32:13 +0530

    Time: 127 ms (83.44%), Space: 26.9 MB (24.5%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -5,30 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        seen = set()           # to track distinct elements in the current window
-        w_sum = 0              # current window sum
+        seen = set()
+        left=0
+        w_sum=0
         max_sum = 0
-        left = 0
-
         for right in range(len(nums)):
-            # If duplicate found, shrink window from the left
             while nums[right] in seen:
                 seen.remove(nums[left])
-                w_sum -= nums[left]
-                left += 1
-
-            # Add new element to window
+                w_sum-=nums[left]
+                left+=1
             seen.add(nums[right])
-            w_sum += nums[right]
+            w_sum+=nums[right]
 
-            # Maintain window size == k
-            if right - left + 1 > k:
+            if right - left + 1>k:
                 seen.remove(nums[left])
-                w_sum -= nums[left]
-                left += 1
-
-            # If valid window of size k, update answer
-            if right - left + 1 == k:
-                max_sum = max(max_sum, w_sum)
-
+                w_sum-=nums[left]
+                left+=1
+            if right-left+1 == k:
+                max_sum = max(max_sum,w_sum)
         return max_sum
