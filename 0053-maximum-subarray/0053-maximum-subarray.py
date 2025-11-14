4445cc04faab8bb447d6cf67e340788ccd375cc7
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2025-11-14 19:18:11 +0530

    Time: 57 ms (73.85%), Space: 21.9 MB (36.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,8 +1,18 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        res = nums[0]
-        ends = nums[0] 
-        for i in range(1,len(nums)):
-            ends = max(ends+nums[i],nums[i])
-            res = max(ends,res)
-        return res
+        max_sum = nums[0]
+        curr_sum = 0
+
+        for num in nums:
+            # Expand window
+            curr_sum += num
+
+            # Update global maximum
+            max_sum = max(max_sum, curr_sum)
+
+            # Shrink/reset window when it becomes harmful
+            if curr_sum < 0:
+                curr_sum = 0   # drop window completely
+
+        return max_sum
+        
