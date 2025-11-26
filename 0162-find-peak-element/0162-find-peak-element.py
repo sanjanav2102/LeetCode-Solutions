d913cddd0a201f7b4bbae1ba7fa753c40d2eea97
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2025-11-26 17:07:06 +0530

    Time: 0 ms (100%), Space: 12.5 MB (79.5%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,16 +1,33 @@
 class Solution(object):
     def findPeakElement(self, nums):
         l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if nums[mid] < nums[mid + 1]:
+                # peak is on the right side
+                l = mid + 1
+            else:
+                # peak is on the left side (including mid)
+                r = mid
+
+        # left == right → peak index
+        return l
+
+'''class Solution(object):
+    def findPeakElement(self, nums):
+        l = 0
         for h in range(1,len(nums)):
             if nums[h-1] <= nums[h]:
                # l+=1
                h+=1
                if h == len(nums):
-                return h-1
-                
+                return h-1    
             else:
                 return h-1
         else:
-            return 0
+            return 0'''
 
         
