2e8073159fd351d50b54462f4c63652abadcdbbc
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2026-05-03 15:12:08 +0530

    Time: 259 ms (21.42%), Space: 29.4 MB (25.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,40 @@
 class Solution(object):
     def numIslands(self, grid):
-        row = len(grid)
-        col = len(grid[0])
-        no = 0
-        def dfs(r,c):
-            if r<0 or r>=row or c<0 or c>=col or grid[r][c] == '0':
-                return 
-            grid[r][c] = '0' #visited
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1':
-                    dfs(i,j)
-                    no+=1
-        return no
+        def bfs(i,j,vis,grid):
+            q=[]
+            q.append([i,j])
+            vis[i][j] = 1
+            while q:
+                i = q[0][0]
+                j = q[0][1]
+                q.pop(0)
+                #traverse neighbors
+                # see image in docs
+                #for deli in range (-1,2):
+                 #   for delj in range (-1,2):
+                  #      ni = i+deli
+                   #     nj = j+delj
+                for deli, delj in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    ni = i + deli
+                    nj = j + delj
+                    if (ni >=0 and ni < n and nj>=0 and nj<m and grid[ni][nj] == '1' and not vis[ni][nj]):
+                        vis[ni][nj] = 1
+                        q.append([ni,nj])
+
+        n = len(grid)
+        m = len(grid[0])
+        c =0
+        #vis = [([0]*m)]*n
+         # ✅ fix visited matrix
+        vis = [[0]*m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if not vis[i][j] and grid[i][j] == '1':
+                    c+=1
+                    bfs(i,j,vis,grid)
+        return c
+
+
+
+         
+        
