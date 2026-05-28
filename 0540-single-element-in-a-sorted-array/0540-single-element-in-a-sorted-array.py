6d49e7cb1a8afdf1953869a528fa3fad884186ed
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2026-05-28 13:30:33 +0530

    Time: 11 ms (11.13%), Space: 18.7 MB (11.03%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i]+=1
+        for i in d:
+            if d[i] == 1:
+                return i
