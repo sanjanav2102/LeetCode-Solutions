e3285c172f5159fe65cd6cfc631e05491238fc9c
Sanjana Viswanathan <85557735+sanjanav2102@users.noreply.github.com>
2026-06-29 19:31:04 +0530

    Time: 3321 ms (6.56%), Space: 12.4 MB (49.18%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.py
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.py
@@ -1,22 +1,24 @@
-
 class Solution(object):
-    def findGcd(self,a,b):
-        while b!=0 :
-            a,b = b,a%b
+    def findgcd(self,a,b):
+        while b!= 0:
+            a,b = b, a%b
         return a 
-    def findLcm(self,a,b):
-        return (a*b)//self.findGcd(a,b)
+    def findlcm(self,a,b):
+        return (a*b)/self.findgcd(a,b)
     def subarrayLCM(self, nums, k):
         c = 0
         for i in range(len(nums)):
-            lcm = 1 #minimum reset value 1
+            lcm = 1
             for j in range(i,len(nums)):
-                lcm = self.findLcm(lcm,nums[j])
+                lcm = self.findlcm(lcm,nums[j])
                 if lcm == k:
                     c+=1
-                elif lcm>k:
-                    break
+            
         return c
-    
-        
-        
+
+           
+
+            
+       
+
+
